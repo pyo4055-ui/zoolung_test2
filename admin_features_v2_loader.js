6514c5bbeb34b3205ef6842f9d1e52839b7dbbc8
pyo4055-ui <pyo4055@gmail.com>
2026-08-23 14:30:09 +0900

    fix: load admin tab active state cleanup

--- admin_features_v2_loader.js
+++ admin_features_v2_loader.js
@@ -187,7 +187,8 @@ function loadCustomerQuickTools(){
     ['zrCustomerStatusBannerV1','./customer_status_banner_v1.js?v=1'],
     ['zrCustomerGuideMapAdminUiV2','./customer_guide_map_admin_ui_v2.js?v=1'],
     ['zrCustomerTimeGuideGuardV2','./customer_time_guide_guard_v2.js?v=1'],
-    ['zrAdminCancelVisibilityV1','./admin_cancel_visibility_v1.js?v=1']
+    ['zrAdminCancelVisibilityV1','./admin_cancel_visibility_v1.js?v=1'],
+    ['zrAdminTabActiveFixV1','./admin_tab_active_fix_v1.js?v=1']
   ];
   for(const [id,src] of tools){
     if(document.getElementById(id))continue;
