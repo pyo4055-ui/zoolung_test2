7f857a08a26e1f193473c12263d5ca228e9c6709
pyo4055-ui <pyo4055@gmail.com>
2026-08-23 17:40:12 +0900

    chore: bump activity query fix cache version

--- admin_features_v2_loader.js
+++ admin_features_v2_loader.js
@@ -189,7 +189,7 @@ function loadCustomerQuickTools(){
     ['zrCustomerTimeGuideGuardV2','./customer_time_guide_guard_v2.js?v=1'],
     ['zrAdminCancelVisibilityV1','./admin_cancel_visibility_v1.js?v=1'],
     ['zrAdminTabActiveFixV1','./admin_tab_active_fix_v1.js?v=1'],
-    ['zrAdminActivityFilterFixV1','./admin_activity_filter_fix_v1.js?v=2']
+    ['zrAdminActivityFilterFixV1','./admin_activity_filter_fix_v1.js?v=3']
   ];
   for(const [id,src] of tools){
     if(document.getElementById(id))continue;
