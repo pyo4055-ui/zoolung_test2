0439c87c646c19cf422fdaaa95c00b939336dc8d
pyo4055-ui <pyo4055@gmail.com>
2026-08-24 08:42:23 +0900

    fix: contain iOS admin date inputs

--- admin_mobile_date_input_fix_v1.js
+++ admin_mobile_date_input_fix_v1.js
@@ -12,9 +12,23 @@ function injectStyle(){
       #adminView input[type="date"]{
         display:block!important;
         width:100%!important;
+        width:-webkit-fill-available!important;
+        inline-size:-webkit-fill-available!important;
         max-width:100%!important;
+        max-inline-size:100%!important;
         min-width:0!important;
+        min-inline-size:0!important;
         box-sizing:border-box!important;
+        margin-left:0!important;
+        margin-right:0!important;
+        overflow:hidden!important;
+        justify-self:stretch!important;
+      }
+      #adminView input[type="date"]::-webkit-date-and-time-value{
+        width:100%!important;
+        min-width:0!important;
+        margin:0!important;
+        text-align:center!important;
       }
     }
   `;
